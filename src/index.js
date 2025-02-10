8c402338e2a8ebe09fe615f24c13f53a16f52101
dolapojames <oludolapo@hotmail.com>
2025-02-10 13:01:06 +0000

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "5ca9a4e04df3dddde0tdc3bec6cd3f5o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormCity = document.querySelector("#search-form");
 searchFormCity.addEventListener("submit", handleSearchSubmit);
-alert("hello");
+
+searchCity("Abuja");
