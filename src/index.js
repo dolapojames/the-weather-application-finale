cd2797967343ec3f17540358b57c51f6ebfdd833
dolapojames <oludolapo@hotmail.com>
2025-02-11 11:51:00 +0000

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -5,6 +5,42 @@ function updateWeather(response) {
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  let description = document.querySelector("#description");
+  let currentDescription = response.data.condition.description;
+  description.innerHTML = currentDescription;
+
+  let humidity = document.querySelector("#humidity");
+  let currentHumidity = `${response.data.temperature.humidity}%`;
+  humidity.innerHTML = currentHumidity;
+
+  let windspeed = document.querySelector("#windspeed");
+  let currentWindSpeed = `${response.data.wind.speed}km/h`;
+  windspeed.innerHTML = currentWindSpeed;
+
+  let time = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  time.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let day = date.getDay();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `${minutes}`;
+  }
+  let hours = date.getHours();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let currentDay = days[day];
+  return `${currentDay} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
